8bb5f62209f63a21ee755390cb43bb2da9049619
Aditya Singh <aadi21082003@gmail.com>
2026-04-06 13:42:20 +0530

    ad

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use('/admin', adminRoutes);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // SPA routing fix
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
